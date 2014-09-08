cb74bb25095e0cdf1577e777cb2450eb00ad2958
Apirak Panatkool <apirakb@gmail.com>
2014-09-09 05:59:52 +0700

    move time view when select time type

--- SprintTimer/UXAViewController.m
+++ SprintTimer/UXAViewController.m
@@ -79,22 +79,23 @@
 -(void)selectTimerType:(id)sender {
     UIButton *button = (UIButton *)sender;
     if(button.tag == 1){
-        [self setTimerName:@"Timer_button_selected.png" Crazy8Name:@"Crazy8_button.png" BarXPoition:50];
-        
+        [self setTimerName:@"Timer_button_selected.png" Crazy8Name:@"Crazy8_button.png" BarXPoition:50 TimerViewXPosition:(1024-UXA_TIMER_SIZE)/2];
     } else {
-        [self setTimerName:@"Timer_button.png" Crazy8Name:@"Crazy8_button_selected.png" BarXPoition:196];
+        [self setTimerName:@"Timer_button.png" Crazy8Name:@"Crazy8_button_selected.png" BarXPoition:196 TimerViewXPosition:(0-UXA_TIMER_SIZE)];
     }
 }
 
--(void)setTimerName:(NSString*)timerName Crazy8Name:(NSString*)crazy8Name BarXPoition:(NSInteger)barXPosition {
+-(void)setTimerName:(NSString*)timerName Crazy8Name:(NSString*)crazy8Name BarXPoition:(NSInteger)barXPosition TimerViewXPosition:(NSInteger)timerViewXPosition {
     [timerButton setBackgroundImage:[[UIImage imageNamed:timerName]
                                      stretchableImageWithLeftCapWidth:10.0 topCapHeight:0.0] forState:UIControlStateNormal];
     [crazyButton setBackgroundImage:[[UIImage imageNamed:crazy8Name]
                                      stretchableImageWithLeftCapWidth:10.0 topCapHeight:0.0] forState:UIControlStateNormal];
     [UIView beginAnimations:@"MoveView" context:nil];
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
-    [UIView setAnimationDuration:0.2f];
+//    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+    [UIView setAnimationDuration:0.5f];
     [tabLineBarView setFrame:CGRectMake(barXPosition, 80, 120, 6)];
+    [timerView setFrame:CGRectMake(timerViewXPosition ,120,UXA_TIMER_SIZE, UXA_TIMER_SIZE)];
     [UIView commitAnimations];
 }
 
